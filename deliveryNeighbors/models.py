6b7536bdccc7d8f233eafa089634741d91f08483
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-05-05 15:16:30 +0900

    feat: User 모델 생성

--- deliveryNeighbors/models.py
+++ deliveryNeighbors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    nickname = models.CharField('NICKNAME', max_length=20)
+    email = models.CharField('EMAIL', max_length=50)
+    pwd = models.CharField('PWD', max_length=30)
+    profile_img = models.ImageField('PROFILE_IMG')
+    created_at = models.DateTimeField('CREATE DATE', auto_now_add=True)
