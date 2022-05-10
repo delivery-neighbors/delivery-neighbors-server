ff2531ad2ea520663da29a91a56d1691ea6fc467
sa46lll <9812smh@naver.com>
2022-05-10 23:59:08 +0900

    feat: 유저 정보 조회

--- config/views.py
+++ config/views.py
@@ -1,3 +1,5 @@
+import urllib
+
 import requests
 from django.shortcuts import redirect
 
@@ -32,11 +34,12 @@ def kakao_callback(request):
 
     # 2 POST 방식
     code = request.GET.get('code')
+    print(f"코드: {code}")
     token_api = "https://kauth.kakao.com/oauth/token"  # 토큰 받기 api
     data = {
         'grant_type': 'authorization_code',
         'client_id': SOCIAL_OAUTH_CONFIG['KAKAO_REST_API_KEY'],
-        'redirect_url': SOCIAL_OAUTH_CONFIG['KAKAO_REDIRECT_URI'],
+        'redirect_uri': SOCIAL_OAUTH_CONFIG['KAKAO_REDIRECT_URI'],
         'client_secret': SOCIAL_OAUTH_CONFIG['KAKAO_SECRET_KEY'],
         'code': code
     }
@@ -45,9 +48,32 @@ def kakao_callback(request):
     }
 
     token_response = requests.post(token_api, data=data, headers=headers)
-    print("토큰 응답: {}".format(token_response))
+    print(f"토큰 응답: {token_response}")
     token_json = token_response.json()
-    print("토큰 응답 json 변환: {}".format(token_json))
+    print(f"토큰 응답 json 변환: {token_json}")
 
-    return 0
+    # access_token 으로 유저 정보 조회
+    access_token = token_json['access_token']
+    user_api = "https://kapi.kakao.com/v2/user/me"  # 사용자 정보 조회 api
+    auth = f"Bearer {access_token}"
+    header = {
+        "Authorization": auth
+        # 'Content-type': 'application/x-www-form-urlencoded;charset=utf-8'
+    }
+    user_request = requests.get(user_api, headers=header)
+    print(f"유저 정보 응답: {user_request}")
+    user_json = user_request.json()
+    print(f"유저 정보 json 변환: {user_json}")
+
+    # 유저 디테일 정보
+    kakao_account = user_json['kakao_account']
+
+    user_email = kakao_account['email']
+
+
+
+    # return Response(res.text)
+    # params = urllib.parse.urlencode(request.GET)
+    # return redirect(f'http://localhost:8070/accounts/kakao/login/callback?{params}')
+    # return 0
 
