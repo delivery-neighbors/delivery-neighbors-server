f60b758cfb903994a8ca0f1b0b5b91c2e6c9f5c1
sa46lll <9812smh@naver.com>
2022-05-15 21:26:09 +0900

    feat: AbstractBaseUser 모델 수정

--- deliveryNeighbors/models.py
+++ deliveryNeighbors/models.py
@@ -1,12 +1,58 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.db import models
 
 
-class User(models.Model):
-    nickname = models.CharField('NICKNAME', max_length=20)
-    email = models.CharField('EMAIL', max_length=50)
-    pwd = models.CharField('PWD', max_length=30)
-    profile_img = models.ImageField('PROFILE_IMG', upload_to='images/%Y/%m/%d/')
-    created_at = models.DateTimeField('CREATE DATE', auto_now_add=True)
+class UserManager(BaseUserManager):
 
-    def __str__(self):
-        return self.email
+    use_in_migrations = True
+
+    def create_user(self, email, username, password=None):
+
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            username=username,
+            password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+
+    objects = UserManager()
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    username = models.CharField(
+        max_length=20,
+        null=False,
+        unique=True
+    )
+    profile_img = models.ImageField(upload_to="images/%Y/%m/%d/")
+
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True, editable=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
