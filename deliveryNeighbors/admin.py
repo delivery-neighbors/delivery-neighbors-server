4b3b51f2e72b22f49a82bbd8c8f249e0e0170f22
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-05-16 14:42:12 +0900

    feat: admin 페이지 수정

--- deliveryNeighbors/admin.py
+++ deliveryNeighbors/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'profile_img']
+    fields = ['username', 'email', 'profile_img']
+
+
+admin.site.register(User, UserAdmin)
