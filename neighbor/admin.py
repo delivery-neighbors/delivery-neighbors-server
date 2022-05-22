303e6cf2ae0146eca09cc94e2b5b751fa140dbb3
sa46lll <9812smh@naver.com>
2022-05-22 09:49:13 +0900

    feat: admin 커스터마이징

--- neighbor/admin.py
+++ neighbor/admin.py
@@ -1,11 +1,30 @@
 from django.contrib import admin
 
+from accounts.models import User
 from neighbor.models import Review, UserReview
+
+
+class UserReviewInline(admin.TabularInline):
+    model = UserReview
+    extra = 1
+
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = (UserReviewInline,)
+    list_display = ['id', 'email', 'username', 'date_joined']
+    list_display_links = ['id', 'email']  # 링크로 표시할 필드
 
 
 class ReviewAdmin(admin.ModelAdmin):
     list_display = ['id', 'content']
 
 
+class UserReviewAdmin(admin.ModelAdmin):
+    list_display = ['user_id', 'review_id', 'count']
+    list_filter = ['user_id']  # 필터 처리된 user_id를 우측에 보여줌
+    ordering = ('-user_id', '-review_id', )
+
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Review, ReviewAdmin)
-admin.site.register(UserReview)
+admin.site.register(UserReview, UserReviewAdmin)
