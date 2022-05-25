1ff31b4bae4b4b2440cde0205e1fddda44309406
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-05-25 14:49:49 +0900

    feat: 채팅방 목록 조회,채팅방 생성, 채팅방 단일 조회, 채팅방 삭제, 유저 채팅방 참여

--- chat/urls.py
+++ chat/urls.py
@@ -3,8 +3,12 @@ from chat import views
 
 app_name = 'chat'
 urlpatterns = [
-    path('room/', views.RoomGetCreateAPIView.as_view(), name='room_create'),
-    path('room/<int:pk>', views.RoomGetDestroyAPIView.as_view(), name='room-destroy'),
+    # 채팅방 생성 & 목록 조회
+    path('room/', views.RoomGetCreateAPIView.as_view(), name='room-main'),
+    # 단일 채팅방 조회 & 삭제
+    path('room/<int:pk>', views.RoomRetrieveDestroyAPIView.as_view(), name='room-sub'),
+    # 카테고리 조회
     path('category/', views.CategoryListView.as_view(), name='category-list'),
-    # path('category/create', views.CategoryCreateView.as_view(), name='category-create'),
+    # 유저 채팅방 참여
+    path('room/<int:room_id>/users/', views.ChatUserView.as_view(), name='chat_user'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.generics import *
 from rest_framework.renderers import JSONRenderer
 from rest_framework_simplejwt.authentication import JWTAuthentication
@@ -6,30 +7,64 @@ from rest_framework import status
 
 import config.authentication
 from accounts.models import User
-from chat.models import Category, Room
+from chat.models import Category, Room, ChatUser
 from chat.serializers import CategoryListSerializer, RoomListSerializer, RoomRetrieveSerializer
 from config.authentication import CustomJWTAuthentication
+
+from haversine import haversine, Unit
 
 
 class RoomGetCreateAPIView(ListCreateAPIView):
     def get(self, request):
-        queryset = Room.objects.all()
-        serializer = RoomListSerializer(instance=queryset, many=True)
+        request_latitude = float(request.GET['user_latitude'])
+        request_longitude = float(request.GET['user_longitude'])
+        request_location = (request_latitude, request_longitude)
+        print("------------------------------------")
+        print("request location", request_location)
+
+        # filter()에 넣어줄 조건
+        # 1km에 대해 위도는 0.01 차이, 경도는 0.015 차이
+        # 즉, request 통해 받아온 위치(위도,경도)에서 +,- 0.005 (위도) / +,- 0.0075 (경도) 떨어진 곳까지 1차 필터링
+        # Q 클래스 -> filter()에 넣어줄 논리 조건을 | 또는 & 사용해 조합 가능케 해줌
+        condition = (
+                Q(pickup_latitude__range=(request_latitude - 0.005, request_latitude + 0.005)) &
+                Q(pickup_longitude__range=(request_longitude - 0.0075, request_longitude + 0.0075))
+        )
+        rooms_first_filtering = Room.objects.filter(condition)
+
+        # 500m 이내 채팅방 2차 필터링
+        rooms_within_500meters = [room for room in rooms_first_filtering
+                                  if haversine(request_location,
+                                               (room.pickup_latitude, room.pickup_longitude),
+                                               unit=Unit.METERS) < 500]
+
+        for room in rooms_within_500meters:
+            distance = haversine(request_location, (room.pickup_latitude, room.pickup_longitude), unit=Unit.METERS)
+            room_id = room.id
+            room = room.__dict__
+            room['distance'] = int(distance)
+
+            # 현재 채팅방 참여자 수 추출
+            participant_num = ChatUser.objects.filter(room_id=room_id).count()
+            # 현재 채팅방 참여자 수 정보 추가를 위해 room 객체를 dict 로 변환 뒤
+            # 해당 dict 에 participant_num key-value 추가
+            # -> room 객체에 participant_num 정보 자동 삽입
+            room['participant_num'] = participant_num
+
+        serializer = RoomListSerializer(instance=rooms_within_500meters, many=True)
         return Response(serializer.data)
 
     def post(self, request):
         # request 에서 user_id 추출
         leader = CustomJWTAuthentication.authenticate(self, request)
-        print("user", leader)
+        print("------------------------------------")
+        print("RoomCreate", leader, "번 유저가 채팅방 생성")
 
         # 요청 받은 category id로 해당 카테고리 객체 get
         cate_id = request.data['category']
-        print("cate_id", cate_id)
         category = Category.objects.get(id=cate_id)
-        print("category type", type(category))
-        print("category", category)
 
-        Room.objects.create(
+        room = Room.objects.create(
             leader=User.objects.get(id=leader),
             room_name=request.data['room_name'],
             category=Category.objects.get(id=cate_id),
@@ -41,28 +76,39 @@ class RoomGetCreateAPIView(ListCreateAPIView):
             pickup_longitude=request.data['pickup_longitude']
         )
 
+        ChatUser.objects.create(
+            room=room,
+            user=User.objects.get(id=leader)
+        )
+
         return Response(status=status.HTTP_201_CREATED)
 
 
-class RoomGetDestroyAPIView(RetrieveDestroyAPIView):
+class RoomRetrieveDestroyAPIView(RetrieveDestroyAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomRetrieveSerializer
 
     def get(self, request, *args, **kwargs):
         # path-variable <int:pk> 정보를 통해 해당 id의 room 객체 get
         room = self.get_object()
+        room_id = room.pk
+
+        # 현재 채팅방 참여자 수 추출
+        participant_num = ChatUser.objects.filter(room_id=room_id).count()
+        # 현재 채팅방 참여자 수 정보 추가를 위해 room 객체를 dict 로 변환 뒤
+        participant_detail = room.__dict__
+        # 해당 dict 에 participant_num key-value 추가
+        # -> room 객체에 participant_num 정보 자동 삽입
+        participant_detail['participant_num'] = participant_num
 
         # RoomRetrieveSerializer 이용해
-        serializer = self.serializer_class
-        # retrieve()
-        return self.retrieve(request, *args, **kwargs)
+        serializer = self.serializer_class(instance=room)
+        return Response(serializer.data)
 
     def delete(self, request, *args, **kwargs):
         user = CustomJWTAuthentication.authenticate(self, request)
-        print("user", user)
         room = self.get_object()
         room_leader = room.leader.pk
-        print("room leader", room_leader)
 
         # 채팅방 leader 와  로그인 유저가 같으면 삭제 가능
         if user == room_leader:
@@ -71,13 +117,29 @@ class RoomGetDestroyAPIView(RetrieveDestroyAPIView):
 
         else:
             # 다르면 401 UNAUTHORIZED 에러 응답
+            print("------------------------------------")
+            print("Room Created User != Login User")
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-
-
-# class CategoryCreateView(CreateAPIView):
-#     serializer_class = CategoryCreateSerializer
 
 
 class CategoryListView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategoryListSerializer
+
+
+class ChatUserView(ListCreateAPIView):
+    queryset = ChatUser.objects.all()
+
+    def post(self, request, room_id):
+        # path-variable <int:room_id>로 받아온 채팅방 인덱스 통해 Room 객체 get
+        room = Room.objects.get(id=room_id)
+        # 로그인 유저 pk
+        user_pk = CustomJWTAuthentication.authenticate(self, request)
+
+        # serializer 없이 직접 생성
+        ChatUser.objects.create(
+            room=room,
+            user=User.objects.get(id=user_pk)
+        )
+
+        return Response(status=status.HTTP_201_CREATED)
