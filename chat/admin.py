6ef0b05924a0a5c05556b90955593a7ec3dfa79b
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-05-25 14:46:57 +0900

    feat: chat 관련 모델 admin 등록

--- chat/admin.py
+++ chat/admin.py
@@ -6,6 +6,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ['id', 'category_name']
 
 
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['id', 'leader', 'room_name', 'created_at', 'is_active']
+
+
+class ChatUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'room', 'user']
+
+
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Room)
-admin.site.register(ChatUser)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(ChatUser, ChatUserAdmin)
