f22c75a37c4c177c86c0e5919b4e0474dc358e81
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-05-25 14:48:56 +0900

    feat: 채팅방 목록 조회, 채팅방 단일 조회 시리얼라이저

--- chat/serializers.py
+++ chat/serializers.py
@@ -4,41 +4,52 @@ from chat.models import Room, Category
 from config.authentication import CustomJWTAuthentication
 
 
+# 채팅방 목록(메인 화면) serializer
 class RoomListSerializer(serializers.ModelSerializer):
+    distance = serializers.FloatField()
+    leader_avatar = serializers.URLField(source='leader.avatar')
+    participant_num = serializers.IntegerField()
+
+    # todo created_at 가공 후 내보낼 수 있는지 알아 보기
+
     class Meta:
         model = Room
         fields = [
             'id',
             'leader',
+            'leader_avatar',
             'room_name',
             'created_at',
+            'participant_num',
             'max_participant_num',
-            'delivery_fee'
+            'delivery_fee',
+            'distance'
         ]
 
 
+# 채팅방 정보 serializer
 class RoomRetrieveSerializer(serializers.ModelSerializer):
     category_background_img = serializers.URLField(source='category.category_background_img')
+    leader_avatar = serializers.URLField(source='leader.avatar')
+    participant_num = serializers.IntegerField()
 
     class Meta:
         model = Room
         fields = [
             'id',
             'leader',
+            'leader_avatar',
             'room_name',
             'delivery_fee',
+            'participant_num',
             'max_participant_num',
             'created_at',
             'pickup_address',
             'category_background_img'
         ]
 
-# class CategoryCreateSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Category
-#         fields = ['category_name']
 
-
+# 카테고리 serializer
 class CategoryListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
