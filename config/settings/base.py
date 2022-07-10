e64deb574630801e7736e6c7267795b3391d15f6
sa46lll <9812smh@naver.com>
2022-07-10 22:04:40 +0900

    chore: media 경로 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media
-MEDIA_ROOT = os.path.join(BASE_DIR, '../../media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Default primary key field type
