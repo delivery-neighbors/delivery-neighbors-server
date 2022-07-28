b1231072c7433c4fa732ef2a840ea92bcebf2fa7
sa46lll <9812smh@naver.com>
2022-07-28 17:52:33 +0900

    chore: html 경로 변경

--- chatting/views.py
+++ chatting/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 
-from chat.models import ChatUser
-
-print("views.py")
-
 
 def index(request):
-    return render(request, 'chat/index.html')  # render - 전달 받은 이름의 템플릿 HttpResponse 형태로 응답
+    return render(request, 'ws_index.html')  # render - 전달 받은 이름의 템플릿 HttpResponse 형태로 응답
 
 
 def room(request, room_name, user_id):
-    return render(request, 'chat/room.html', {
+    return render(request, 'ws_room.html', {
         'room_name': room_name,
         'user_id': user_id
     })
