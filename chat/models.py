cd4ca80c375e12202df6c6b41a8e9d446dab0c69
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-08-09 00:10:40 +0900

    feat: model-Category 배경사진, User 프로필사진 부분 수정

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     category_name = models.CharField(max_length=32)
-    category_background_img = models.ImageField(default='category/1.jpg', upload_to='images/category')
+    category_background_img = models.URLField()
 
 
 class ChatUser(models.Model):
