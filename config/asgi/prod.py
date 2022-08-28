d5ba6fc37e80dbfcfc7b414064f462b7e08694d8
sa46lll <9812smh@naver.com>
2022-08-28 17:39:03 +0900

    test: daphne 추가

--- config/asgi/prod.py
+++ config/asgi/prod.py
@@ -13,7 +13,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 django_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": django_application,
+    "https": django_application,
     "websocket": AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
