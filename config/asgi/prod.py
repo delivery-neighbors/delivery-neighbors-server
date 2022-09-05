af8013ba4342737b691752100ba920e8f312c6c4
sa46lll <9812smh@naver.com>
2022-09-05 15:34:09 +0900

    test: asgi 변경

--- config/asgi/prod.py
+++ config/asgi/prod.py
@@ -1,6 +1,7 @@
 import os
 
 import django
+from django.core.asgi import get_asgi_application
 
 import chatting
 
@@ -12,9 +13,11 @@ from chatting import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.prod')
 django.setup()
-application = get_default_application()
+
+django_application = get_asgi_application()
 
 application = ProtocolTypeRouter({
+    "http": django_application,
     "websocket": AuthMiddlewareStack(
             URLRouter(
                 chatting.routing.websocket_urlpatterns
