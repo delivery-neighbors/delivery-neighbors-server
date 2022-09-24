da8f1c7e57844137aff295b8a86ef2a87d81dd69
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-09-25 08:50:22 +0900

    feat: 추천 이웃 채팅방

--- recommendation/serializers.py
+++ recommendation/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from chat.models import Room
 from neighbor.models import OrderFrequency
 
 
@@ -7,3 +8,23 @@ class OrderFrequencySerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderFrequency
         fields = '__all__'
+
+
+class SimilarUserChatroomSerializer(serializers.ModelSerializer):
+    distance = serializers.IntegerField()
+    leader_avatar = serializers.ImageField(source='leader.avatar')
+    participant_num = serializers.IntegerField()
+
+    class Meta:
+        model = Room
+        fields = [
+            'id',
+            'leader_avatar',
+            'room_name',
+            'created_at',
+            'participant_num',
+            'max_participant_num',
+            'delivery_fee',
+            'distance',
+            'status'
+        ]
