3ecfdd1c6aebaccb72bf912a9ee6d94f2fed2e42
sa46lll <9812smh@naver.com>
2022-11-04 12:27:56 +0900

    feat: 채팅 메세지 목록 조회

--- chatting/serializers.py
+++ chatting/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from accounts.models import User
+from chatting.models import Message
 
 
 class ChattingUserSerializer(serializers.ModelSerializer):
@@ -12,3 +13,13 @@ class ChattingUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['room_id', 'user_id', 'chat_user_id', 'username', 'user_avatar']
+
+
+class MessageListSerializer(serializers.ModelSerializer):
+    user_id = serializers.IntegerField(source='chat_user.user.id')
+    username = serializers.CharField(source='chat_user.user.username')
+    user_avatar = serializers.CharField(source='chat_user.user.avatar')
+
+    class Meta:
+        model = Message
+        fields = ['id', 'user_id', 'chat_user', 'username', 'user_avatar', 'message']
