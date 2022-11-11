b4b387701e6d60da069ca9820d5848955490e4c4
jwUm <87863185+umjiwoo@users.noreply.github.com>
2022-11-11 16:07:24 +0900

    feat: 토큰 재발급 for 자동 로그인

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
+
 from accounts import views
 from django.conf import settings
 from django.conf.urls.static import static
@@ -20,4 +22,6 @@ urlpatterns = [
     path('deliveryneighbors/resign/', views.UserResignAPIView.as_view(), name='user-resign'),
 
     path('deliveryneighbors/reset_pwd/', views.PwdResetAPIView.as_view(), name='pwd-reset'),
+
+    path('deliveryneighbors/token/refresh/', TokenRefreshView.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
